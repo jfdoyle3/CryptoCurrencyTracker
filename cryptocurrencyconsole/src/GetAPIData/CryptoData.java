b054313a5e9c37c212aa9d530b376d1f5c0eb72a
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2020-05-29 10:28:31 -0400

    added server, update pom.xml, added code again

---
+++ cryptocurrencyconsole/src/GetAPIData/CryptoData.java
@@ -0,0 +1,44 @@
+package GetAPIData;
+
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+import com.google.gson.JsonArray;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+import com.google.gson.JsonParser;
+import com.jayway.jsonpath.JsonPath;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+
+public class CryptoData {
+
+	public static void getCryptoData() {
+
+		
+		String oneJson="[{\"exchange\":\"coinbene\",\"market\":\"BETHER_ETH\",\"base\":\"BETHER\",\"quote\":\"ETH\"}";
+		final   HttpResponse<String> jsonResponse = Unirest.get("https://api.nomics.com/v1/markets").queryString("key", "").asString();
+		
+		//String[] arrJson=stringJson.split("\\n");
+		Gson gson = new Gson();
+		//String json = gson.toJson(arrJson[0]);
+		//System.out.println(arrJson[]);
+		
+	//	for(int idx=0; idx<arrJson.length; idx++) {
+	//		PotHole data = new Gson().fromJson(arrJson[idx], PotHole.class);
+	//		potholeList.add(data);
+	//	}
+		//PotHole address=gsonBuild.fromJson(json, PotHole.class);
+		
+		//System.out.println(data.toString());
+		
+		 System.out.println(jsonResponse.getBody());
+	}
+}

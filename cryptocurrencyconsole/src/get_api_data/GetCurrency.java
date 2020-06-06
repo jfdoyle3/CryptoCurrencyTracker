bdbae785b77cbbe17b581ed48bf1beb4d02d75f9
Jim Doyle <indellibleink@gmail.com>
2020-06-06 01:29:45 -0400

    Getting individual fields on the JSON dataset

--- cryptocurrencyconsole/src/get_api_data/GetCurrency.java
+++ cryptocurrencyconsole/src/get_api_data/GetCurrency.java
@@ -1,44 +1,21 @@
 package get_api_data;
 
-import java.util.ArrayList;
-
-import com.google.gson.Gson;
-
 import kong.unirest.HttpResponse;
 import kong.unirest.Unirest;
 import kong.unirest.json.JSONArray;
-import kong.unirest.json.JSONObject;
-import objects.CurrencyID;
-import objects.CurrencyInfo;
 
 public class GetCurrency {
 
-	public static void Currencies(String currency) {
+	public static JSONArray Currencies(String currency) {
 
 		// ******** REMOVE API KEY before committing *****
 
 		final HttpResponse<String> jsonStringResponse = Unirest.get("https://api.nomics.com/v1/currencies/ticker")
-				.queryString("key", "").queryString("ids", currency)
+				.queryString("key", "8381f81057e8766c11cd0109bae84864").queryString("ids", currency)
 				.queryString("interval", "1d").asString();
 
 		String json = jsonStringResponse.getBody();
-
 		JSONArray currencyJson = new JSONArray(json);
-		JSONObject currencyData0 = currencyJson.getJSONObject(0);
-	//	JSONObject currencyData1 = currencyJson.getJSONObject(1);
-
-		  JSONObject jo = (JSONObject) currencyData0; 
-          
-	        String logoUrl = (String) jo.get("logo_url"); 
-	     
-	          
-	        System.out.println(logoUrl); 
-	        
-	        
-
-		
-	//	System.out.printf("0: %s\n",currencyData0.toString());
-	//	System.out.printf("1: %s",currencyData1.toString());
-		System.out.println("\nend of line");
+		return currencyJson;
 	}
 }
--- cryptocurrencyconsole/src/main/Main.java
+++ cryptocurrencyconsole/src/main/Main.java
@@ -2,17 +2,32 @@ package main;
 
 import database.DBConnection;
 import get_api_data.GetCurrency;
-
+import kong.unirest.json.JSONArray;
+import kong.unirest.json.JSONObject;
 
 public class Main {
 
 	public static void main(String[] args) {
 
-	//	DBConnection.connectDB();
-		
-		GetCurrency.Currencies("BTC,ETH");
+		// DBConnection.connectDB();
 
+		JSONArray json = GetCurrency.Currencies("BTC,ETH");
+
+	
+		for(int idx=0; idx<json.length(); idx++) {
+		JSONObject currencyData = json.getJSONObject(0);
+
+		JSONObject jo = (JSONObject) currencyData;
+
+		String id =  (String) jo.get("id");
+		String currency = (String) jo.get("currency");
+		String name = (String) jo.get("name");
+		String logo = (String) jo.get("logo_url");
+		String symbol = (String) jo.get("symbol");
 		
+		System.out.printf("id: %s\ncurrency: %s\nsymbol: %s\nname: %s\nlogo: %s\n",id,currency,symbol,name,logo);
+		
+		}
 //BTC,ETH,USDT,XRP,BCH,BSV,LTC,BNB,EOS,ADA
 	}
 

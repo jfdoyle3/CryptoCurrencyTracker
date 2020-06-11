d4a574a5e9c9d739ab626c1f236f45d167b6df9d
Jim Doyle <indellibleink@gmail.com>
2020-06-11 17:14:07 -0400

    creating a testing class and the db sql query

---
+++ SQL/create-cryptocurrencytracker-db.sql
@@ -0,0 +1,50 @@
+DROP SCHEMA IF EXISTS `cryptocurrencytracker`;
+
+CREATE SCHEMA `cryptocurrencytracker`;
+
+use `cryptocurrencytracker`;
+
+SET FOREIGN_KEY_CHECKS = 0;
+
+DROP TABLE IF EXISTS `intervals`;
+
+CREATE TABLE `intervals` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price_date` varchar(45) DEFAULT NULL,
+  `price_timestamp` varchar(45) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `currency_info`;
+
+CREATE TABLE `currency_info` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `price` varchar(1000) DEFAULT NULL,
+  `circulating_supply` varchar(1000) DEFAULT NULL,
+  `max_supply` varchar(1000) DEFAULT NULL,
+  `market_cap` varchar(1000) DEFAULT NULL,
+  `ranking` varchar(1000) DEFAULT NULL,
+  `high` varchar(1000) DEFAULT NULL,
+  `high_timestamp` varchar(1000) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+DROP TABLE IF EXISTS `cryptocurrencies`;
+
+CREATE TABLE `cryptocurrencies` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `currencyId` varchar(45) DEFAULT NULL,
+  `currency` varchar(45) DEFAULT NULL,
+  `symbol` varchar(45) DEFAULT NULL,
+  `name` varchar(45) DEFAULT NULL,
+  `logo_url` varchar(45) DEFAULT NULL,
+  `info_id` int(11) DEFAULT NULL,
+  `interval_id` int(11) DEFAULT NULL,
+  PRIMARY KEY (`id`),
+  KEY `FK_INFO_idx` (`info_id`),
+  KEY `FK_INTERVALS_idx` (`interval_id`),
+  CONSTRAINT `FK_INFO` FOREIGN KEY (`info_id`) REFERENCES `currency_info` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+   CONSTRAINT `FK_INTERVALS` FOREIGN KEY (`interval_id`) REFERENCES `intervals` (`id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+) ENGINE=InnoDB AUTO_INCREMENT=1 DEFAULT CHARSET=latin1;
+
+SET FOREIGN_KEY_CHECKS = 1;

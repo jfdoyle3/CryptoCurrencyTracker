013cb6d9d7f2a3789aaf517469be0a8f11e82780
Jim Doyle <indellibleink@gmail.com>
2020-06-11 15:02:12 -0400

    removed comments

--- cryptocurrencyconsole/src/main/EntityDBWrite.java
+++ cryptocurrencyconsole/src/main/EntityDBWrite.java
@@ -12,11 +12,12 @@ public class EntityDBWrite {
 
 	public static void main(String[] args) {
 
-		// create session factory
-		SessionFactory factory = new Configuration().configure().addAnnotatedClass(Cryptocurrencies.class)
-				.addAnnotatedClass(CurrencyInfo.class).addAnnotatedClass(Intervals.class).buildSessionFactory();
+		SessionFactory factory = new Configuration().configure()
+				.addAnnotatedClass(Cryptocurrencies.class)
+				.addAnnotatedClass(CurrencyInfo.class)
+				.addAnnotatedClass(Intervals.class)
+				.buildSessionFactory();
 
-		// create session
 		Session session = factory.getCurrentSession();
 
 		try {
@@ -26,28 +27,24 @@ public class EntityDBWrite {
 			CurrencyInfo currencyInfo = new CurrencyInfo("564.85130437", "18391300", "21000000", "175910049794", "1",
 					"19345.06577687", "2017-12-16T00:00:00Z");
 			Intervals interval = new Intervals("2020-06-01T00:00:00Z", "2020-06-01T14:39:00Z");
-			// associate the objects
+
 			currency.setCurrencyInfo(currencyInfo);
 			currency.setInterval(interval);
-			
 
-			// start a transaction
 			session.beginTransaction();
 
-	
 			System.out.println("||Saving Currency: " + currency);
 			System.out.println("||Info: " + currencyInfo);
 			System.out.println("||Interval: " + interval);
-			session.save(currency);
-			
 
-			// commit transaction
+			session.save(currency);
+
 			session.getTransaction().commit();
 			System.out.println("Done!");
-			
-			
+
 		} catch (Exception err) {
 			err.printStackTrace();
+
 		} finally {
 			session.close();
 			factory.close();

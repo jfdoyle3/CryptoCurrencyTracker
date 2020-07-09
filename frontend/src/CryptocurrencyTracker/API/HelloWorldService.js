5ab2800dea265e58f68d18c38d4ae2b999d1b889
Jim Doyle <indellibleink@gmail.com>
2020-07-09 16:19:46 -0400

    Basic Authentication working

--- frontend/src/CryptocurrencyTracker/API/HelloWorldService.js
+++ frontend/src/CryptocurrencyTracker/API/HelloWorldService.js
@@ -2,22 +2,31 @@ import axios from "axios";
 
 class HelloWorldService {
   executeHelloWorldService() {
-    console.log("executed service");
+    //console.log('executed service')
     return axios.get("http://localhost:8080/hello-world");
   }
 
   executeHelloWorldBeanService() {
     //console.log('executed service')
-    let userName = "user";
-    let password = "password";
-    let basicAuthHeader = "Basic " + window.btoa(userName + ":" + password);
-    return (
-      axios.get("http://localhost:8080/hello-world-bean"),
-      {
-        headers: {
-          authorization: basicAuthHeader,
-        },
-      }
+    return axios.get("http://localhost:8080/hello-world-bean");
+  }
+
+  executeHelloWorldPathVariableService(name) {
+    //console.log('executed service')
+  //  let username = "user";
+   // let password = "password";
+
+    // Standard Basic authenication header in JS.
+   // let basicAuthHeader = "Basic " + window.btoa(username + ":" + password);
+
+    return axios.get(
+      `http://localhost:8080/hello-world/path-variable/${name}`
+	//  ,
+    //  {
+    //    headers: {
+    //      authorization: basicAuthHeader,
+    //    },
+    //  }
     );
   }
 }

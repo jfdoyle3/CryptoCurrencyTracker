193b528b2ad95360a1dd67d17b3a8e1af13907cd
Jim Doyle <indellibleink@gmail.com>
2020-08-17 06:41:14 -0400

    commented out DAO code.

---
+++ backend/src/main/java/com/cryptocurrency/backend/controllers/CurrencyControllerDAO.java
@@ -0,0 +1,32 @@
+package com.cryptocurrency.backend.controllers;
+
+//import java.util.List;
+//
+//import org.springframework.beans.factory.annotation.Autowired;
+//import org.springframework.ui.Model;
+//import org.springframework.web.bind.annotation.CrossOrigin;
+//import org.springframework.web.bind.annotation.GetMapping;
+//import org.springframework.web.bind.annotation.RestController;
+//
+//import com.cryptocurrency.entity.dao.CryptocurrencyDAO;
+//import com.cryptocurrency.entity.objects.Cryptocurrency;
+//
+//@RestController
+//@CrossOrigin(origins = "http://localhost:4200")
+//public class CurrencyController {
+//
+//	@Autowired
+//	//private CryptocurrencyServices cryptocurrencyServices;
+//	private CryptocurrencyDAO cryptocurrencyDAO;
+//	
+//
+//// GET: All Currencies
+//	@GetMapping("/users/{username}/cryptocurrencies")
+//	public String listCurrencies(Model theModel) {
+//	List<Cryptocurrency> currencies=CryptocurrencyDAO.getCurrency();
+//	
+//	theModel.addAttribute("Cryptocurrency", currencies);
+//	
+//	return null;
+//	}
+//}	

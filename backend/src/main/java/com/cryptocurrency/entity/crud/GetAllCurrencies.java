41ac45e86fd17ee7d6618279212f0dc391cd6c6f
Jim Doyle <indellibleink@gmail.com>
2020-08-17 16:24:48 -0400

    Working on session factory error.

--- backend/src/main/java/com/cryptocurrency/entity/crud/GetAllCurrencies.java
+++ backend/src/main/java/com/cryptocurrency/entity/crud/GetAllCurrencies.java
@@ -4,23 +4,54 @@ import java.util.ArrayList;
 import java.util.List;
 
 import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
 
 import com.cryptocurrency.entity.factories.HibernateCryptocurrencyFactory;
 import com.cryptocurrency.entity.objects.Cryptocurrency;
 
 public class GetAllCurrencies {
 
-	@SuppressWarnings("unchecked")
+//	@SuppressWarnings("unchecked")
+//	public static List<Cryptocurrency> getCurrencies() {
+//		List<Cryptocurrency> currencies = new ArrayList<Cryptocurrency>();
+//		Session session = HibernateCryptocurrencyFactory.getSessionFactory().openSession();
+//
+//		session.beginTransaction();
+//		currencies = session.createQuery("from Cryptocurrency").getResultList();
+//
+//		session.getTransaction().commit();
+//		HibernateCryptocurrencyFactory.shutdown();
+//		return currencies;
+//	}
 	public static List<Cryptocurrency> getCurrencies() {
 		List<Cryptocurrency> currencies = new ArrayList<Cryptocurrency>();
-		Session session = HibernateCryptocurrencyFactory.getSessionFactory().openSession();
+	SessionFactory factory = new Configuration().configure()
+			.addAnnotatedClass(Cryptocurrency.class)
+			.buildSessionFactory();
+
+	Session session = factory.getCurrentSession();
+
+	try {
 
 		session.beginTransaction();
+
+		
 		currencies = session.createQuery("from Cryptocurrency").getResultList();
+		session.save(currencies);
 
 		session.getTransaction().commit();
-		HibernateCryptocurrencyFactory.shutdown();
-		return currencies;
+		System.out.println("Done!");
+
+	} catch (Exception err) {
+		err.printStackTrace();
+
+	} finally {
+		session.close();
+		factory.close();
 	}
+	return currencies;
+
+}
 
 }

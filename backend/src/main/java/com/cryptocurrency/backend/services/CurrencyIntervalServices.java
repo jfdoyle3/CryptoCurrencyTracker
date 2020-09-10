cfb4dc7f5f700e964bf97715381ccc3053adfa1c
Jim Doyle <indellibleink@gmail.com>
2020-09-09 22:27:43 -0400

    fixed merge error

--- backend/src/main/java/com/cryptocurrency/backend/services/CurrencyIntervalServices.java
+++ backend/src/main/java/com/cryptocurrency/backend/services/CurrencyIntervalServices.java
@@ -1,7 +1,6 @@
 package com.cryptocurrency.backend.services;
 
-<<<<<<< HEAD
-<<<<<<< HEAD
+
 import java.util.List;
 
 import org.springframework.stereotype.Service;
@@ -53,38 +52,4 @@ public class CurrencyIntervalServices {
 //		return null;
 //}
 }
-=======
 
-=======
-import java.util.ArrayList;
-import java.util.List;
->>>>>>> Working
-
-import org.springframework.stereotype.Service;
-
-import com.cryptocurrency.nomics.api.GetCurrency;
-import com.cryptocurrency.nomics.objects.CurrencyInterval;
-
-import kong.unirest.json.JSONArray;
-
-@Service
-public class CurrencyIntervalServices {
-
-	private static String[] interval = { "7d", "30d", "ytd" };
-
-	public List<CurrencyInterval> getIntervals(String currencySymbol) {
-		String symbol = currencySymbol.toUpperCase();
-
-		List<CurrencyInterval> intervalsList = new ArrayList<>();
-		for (int idx = 0; idx < interval.length; idx++) {
-
-			JSONArray json = GetCurrency.Currencies(symbol, interval[idx]);
-
-			List<CurrencyInterval> currencyInterval = GetCurrency.CurrencyTimeInterval(json, interval[idx]);
-
-			intervalsList.add(currencyInterval.get(0));
-		}
-		return intervalsList;
-	}
-}
->>>>>>> Working

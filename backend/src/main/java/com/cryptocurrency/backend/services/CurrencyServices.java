b9455465cc32a1db753af0f8f5562bd237db8d96
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-06-25 16:29:46 -0400

    creating Services

--- backend/src/main/java/com/cryptocurrency/backend/services/CurrencyServices.java
+++ backend/src/main/java/com/cryptocurrency/backend/services/CurrencyServices.java
@@ -1,5 +1,26 @@
 package com.cryptocurrency.backend.services;
 
+import java.util.List;
+
+import org.springframework.beans.factory.annotation.Value;
+
+import com.cryptocurrency.backend.payload.api.GetCurrency;
+import com.cryptocurrency.backend.payload.response.Cryptocurrency;
+
+import kong.unirest.json.JSONArray;
+
 public class CurrencyServices {
+	
+    @Value("${api.key}")
+    private String apiKey;
+	
+	public List<Cryptocurrency> currencyAPI(String cryptoCurrency, String apiKey) {
+		String interval = "1d";
+		GetCurrency gc=new GetCurrency();
+		JSONArray json = gc.Currencies(cryptoCurrency, interval,apiKey);
+		List<Cryptocurrency> currency=gc.Cryptocurrency(json);
+		
+		return currency;
+	}
 
 }

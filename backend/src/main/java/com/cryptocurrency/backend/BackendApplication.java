2234249740911b1fcf58759b9ece985d051e91a1
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-08-26 17:46:53 -0400

    working with timers

--- backend/src/main/java/com/cryptocurrency/backend/BackendApplication.java
+++ backend/src/main/java/com/cryptocurrency/backend/BackendApplication.java
@@ -4,21 +4,25 @@ import java.util.Date;
 import java.util.Timer;
 import java.util.TimerTask;
 
+import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class BackendApplication {
- private static int counter=0;
+	static int counter=0;
+	static boolean results=false;
 	public static void main(String[] args) {
-		// SpringApplication.run(BackendApplication.class, args);
-		 
+	//	SpringApplication.run(BackendApplication.class, args);
 		 
 		 TimerTask timerTask = new TimerTask() {
 	           @Override
 	           public void run() {
-	        	   counter++;
-	               System.out.println("task  run:"+ new Date()+" counter: "+counter);
+	        	   
+	               System.out.println("task  run:"+ new Date()+" counter: "+counter+" bool: "+results);
+	               counter++;
+	               results=(counter%2==0) ? true : false;
+	            	  
 	           }
 
 	       };
@@ -26,7 +30,8 @@ public class BackendApplication {
 	       Timer timer = new Timer();
 
 	       //Schedule the specified task to start repeated, fixed delays at a specified time.This is executed every 3 seconds
-	       timer.schedule(timerTask,10,3000);
+	       timer.schedule(timerTask,10,1000);
+	       System.out.println("after sch");
 	       
 	   }
 		 		

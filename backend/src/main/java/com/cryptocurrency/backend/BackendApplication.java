93aa92cd7f7fae717cfcb49d82d933e1e985e5ff
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-08-26 15:04:56 -0400

    working on a timer for api calls

--- backend/src/main/java/com/cryptocurrency/backend/BackendApplication.java
+++ backend/src/main/java/com/cryptocurrency/backend/BackendApplication.java
@@ -1,15 +1,35 @@
 package com.cryptocurrency.backend;
 
-import org.springframework.boot.SpringApplication;
+import java.util.Date;
+import java.util.Timer;
+import java.util.TimerTask;
+
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 
 @SpringBootApplication
 public class BackendApplication {
-
+ private static int counter=0;
 	public static void main(String[] args) {
-		 SpringApplication.run(BackendApplication.class, args);
+		// SpringApplication.run(BackendApplication.class, args);
+		 
+		 
+		 TimerTask timerTask = new TimerTask() {
+	           @Override
+	           public void run() {
+	        	   counter++;
+	               System.out.println("task  run:"+ new Date()+" counter: "+counter);
+	           }
+
+	       };
+
+	       Timer timer = new Timer();
+
+	       //Schedule the specified task to start repeated, fixed delays at a specified time.This is executed every 3 seconds
+	       timer.schedule(timerTask,10,3000);
+	       
+	   }
 		 		
-	}
+	
 
 }

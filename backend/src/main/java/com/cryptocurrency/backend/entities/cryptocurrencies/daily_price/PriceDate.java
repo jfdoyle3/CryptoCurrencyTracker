f1f883d99bfa8907355bf1fc57696cf674707639
Jim Doyle <36973560+jfdoyle3@users.noreply.github.com>
2022-09-02 20:42:26 -0400

    Starting to create DBrelationships with DailyPrice

---
+++ backend/src/main/java/com/cryptocurrency/backend/entities/cryptocurrencies/daily_price/PriceDate.java
@@ -0,0 +1,30 @@
+package com.cryptocurrency.backend.entities.cryptocurrencies.daily_price;
+
+import javax.persistence.Column;
+
+public class PriceDate {
+	
+
+	@Column(name = "price_date")
+	private String priceDate;
+
+	@Column(name = "price_timestamp")
+	private String priceTimeStamp;
+	// Price * Circulating Supply = Market Cap
+	@Column(name = "circulating_supply")
+	private String circulatingSupply;
+
+	@Column(name = "max_supply")
+	private String maxSupply;
+	// Market Cap = Price * Circulating Supply
+	@Column(name = "market_cap")
+	private String marketCap;
+
+	@Column(name = "high")
+	private String high;
+
+	@Column(name = "high_timestamp")
+	private String highTimeStamp;
+
+
+}
